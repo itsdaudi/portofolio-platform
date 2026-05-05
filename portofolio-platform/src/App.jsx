c04f40df13b51d0c225415b120f720da31702752
daudikombe <daudikombe77@gmail.com>
2026-05-05 21:20:54 +0300

    deleting of repeating files

--- portofolio-platform/src/App.jsx
+++ portofolio-platform/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import Header from './Components/Header'
+import ProjectList from './Components/ProjectList'
+import ProjectForm from './Components/ProjectForm'
+import SearchBar from './Components/SearchBar'
 import './App.css'
 
 function App() {
